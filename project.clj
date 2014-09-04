f8d41716d8174cb343c105f997707a9c88999722
Hugo Duncan <hugo@hugoduncan.org>
2014-09-04 17:44:14 -0400

    Update to discovery-api 0.1.1

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/palletops/clj-fleet"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-http "1.0.0"]]
+  :dependencies [[com.palletops/discovery-api-runtime "0.1.1"]]
   :profiles
   {:provided {:dependencies [[org.clojure/clojure "1.6.0"]]}
-   :dev {:dependencies [[com.palletops/discovery-api "0.1.0"]]
+   :dev {:dependencies [[com.palletops/discovery-api "0.1.1"]]
          :source-paths ["dev-src"]
          :resource-paths ["dev-resources"]
          :plugins [[lein-pallet-release "RELEASE"]]}
